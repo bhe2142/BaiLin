1a29ada7ebba7b118b73ee41a95f8fa07482b504
Student <student@ent-v321-004.local>
2017-11-09 17:37:22 -0500

    week 11

--- WEEK 10/sketch_oct31b/sketch_oct31b.ino
+++ WEEK 10/sketch_oct31b/sketch_oct31b.ino
@@ -1,25 +1,32 @@
 #include <Servo.h>
 
 Servo base;
-
+Servo base2;
 //digital pin
 int basePin = 9;
+int basePinB = 6;
 
 //analog pin
 int potPin = 5;
+int potPinB = 4;
 void setup() {
  Serial.begin(9600);
 
 base.attach(basePin);
+base2.attach(basePinB);
 }
 
 void loop() {
  int val = analogRead(potPin);
 val = map(val, 0, 1023, 0,180);
 
- Serial.println(val);
+ int val2 = analogRead(potPinB);
+val2 = map(val, 0, 1023, 0,180);
 
+ Serial.println(val);
+ Serial.println(val2);
 base.write(val);
+base2.write(val2);
  delay(10);
 
 }
